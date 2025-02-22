c02d86402cde44143fe3c232cda9fe6ef54661bf
satha-n <netserf@pm.me>
2025-02-21 20:36:49 -0500

    push for auth test

--- src/routes/(protected)/submissions/list/+page.svelte
+++ src/routes/(protected)/submissions/list/+page.svelte
@@ -5,24 +5,5 @@
 </script>
 
 <div class="overflow-x-auto">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Titre</th>
-        <th>Statut</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each data.submissions as submission}
-      <tr>
-        <td>{submission.title}</td>
-        <td>{submission.status}</td>
-        <td>
-          <a href={`submissions/${submission.id}`} class="btn btn-sm">Voir</a>
-        </td>
-      </tr>
-      {/each}
-    </tbody>
-  </table>
+
 </div>
--- src/routes/(protected)/submissions/new/+page.svelte
+++ src/routes/(protected)/submissions/new/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-    import Builder from '$lib/modules/Builder/Builder.svelte';
     import type { PageData } from './$types';
 
     let { data }: { data: PageData } = $props();
 
-    let object = 
 </script>
 
 <div class="">
-  <Builder />
 </div>
